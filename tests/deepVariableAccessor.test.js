036c208a0fb3625a1db70e173571e3810051e75e
_nderscore <_@nderscore.com>
2020-09-25 02:11:21 -0400

    Fix tests.

--- tests/deepVariableAccessor.test.js
+++ tests/deepVariableAccessor.test.js
@@ -1,14 +1,14 @@
 import { runty } from 'runty';
 
-const deepExample = 'Going deep: {%goes.deep} - {%goes.even.0.deeper}';
+const deepExample = 'Going deep: {%goes.deep} - {%goes.even.0.deeper}.';
 
 describe(deepExample, () => {
   const template = runty()(deepExample);
 
   it.each([
-    [undefined, 'Going deep: - .'],
-    [{}, 'Going deep: - .'],
-    [{ goes: {} }, 'Going deep: - .'],
+    [undefined, 'Going deep:  - .'],
+    [{}, 'Going deep:  - .'],
+    [{ goes: {} }, 'Going deep:  - .'],
     [{ goes: { deep: 1 } }, 'Going deep: 1 - .'],
     [{ goes: { deep: 1, even: [] } }, 'Going deep: 1 - .'],
     [{ goes: { deep: 1, even: [{ deeper: 2 }] } }, 'Going deep: 1 - 2.'],
