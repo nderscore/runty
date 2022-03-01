ec0160b3bc73483d185d6a70abea31478ccc8248
_nderscore <_@nderscore.com>
2022-03-01 00:59:52 -0500

    Convert syntax errors tests to typescript / new api

--- tests/syntaxErrors.test.ts
+++ tests/syntaxErrors.test.ts
@@ -1,55 +1,51 @@
 import { RSyntaxErrorType, runty } from 'runty';
 
 describe('Syntax errors', () => {
-  it('should throw EXPECTED_END', () => {
-    const runt = runty({ fns: { foo: () => 0 } });
+  const fns = { foo: () => 0 };
 
+  it('should throw EXPECTED_END', () => {
     expect.assertions(2);
 
     try {
-      runt('This is invalid {%syntax:foo}');
+      runty.string('This is invalid {%syntax:foo}', { fns });
     } catch (e) {
       expect(e.type).toBe(RSyntaxErrorType.EXPECTED_END);
     }
 
     try {
-      runt('This is invalid {$foo()');
+      runty.string('This is invalid {$foo()', { fns });
     } catch (e) {
       expect(e.type).toBe(RSyntaxErrorType.EXPECTED_END);
     }
   });
 
   it('should throw INVALID_EXPRESSION', () => {
-    const runt = runty();
-
     expect.assertions(2);
 
     try {
-      runt('This is invalid {}');
+      runty.string('This is invalid {}');
     } catch (e) {
       expect(e.type).toBe(RSyntaxErrorType.INVALID_EXPRESSION);
     }
 
     try {
-      runt('This is invalid {foo}');
+      runty.string('This is invalid {foo}');
     } catch (e) {
       expect(e.type).toBe(RSyntaxErrorType.INVALID_EXPRESSION);
     }
   });
 
   it('should throw INVALID_FUNCTION', () => {
-    const runt = runty({ fns: { foo: () => 0 } });
-
     expect.assertions(3);
 
     try {
-      runt('This is invalid {$doesntExist()}');
+      runty.string('This is invalid {$doesntExist()}', { fns });
     } catch (e) {
       expect(e.type).toBe(RSyntaxErrorType.INVALID_FUNCTION);
     }
 
     try {
-      runt('This is invalid {$foo($doesntExist())}');
+      runty.string('This is invalid {$foo($doesntExist())}', { fns });
     } catch (e) {
       expect(e.type).toBe(RSyntaxErrorType.INVALID_FUNCTION);
       expect(e.value).toBe('doesntExist');
@@ -57,36 +53,30 @@ describe('Syntax errors', () => {
   });
 
   it('should throw NESTING_DEPTH', () => {
-    const runt = runty({ maxDepth: 3 });
-
     expect.assertions(1);
 
     try {
-      runt('This is invalid {%a?{%b?{%c?{%d}}}}');
+      runty.string('This is invalid {%a?{%b?{%c?{%d}}}}', { maxDepth: 3 });
     } catch (e) {
       expect(e.type).toBe(RSyntaxErrorType.NESTING_DEPTH);
     }
   });
 
   it('should throw UNTERMINATED_EXPRESSION', () => {
-    const runt = runty();
-
     expect.assertions(1);
 
     try {
-      runt('This is invalid {%a?{%b?a}');
+      runty.string('This is invalid {%a?{%b?a}');
     } catch (e) {
       expect(e.type).toBe(RSyntaxErrorType.UNTERMINATED_EXPRESSION);
     }
   });
 
   it('should throw UNTERMINATED_FUNCTION', () => {
-    const runt = runty({ fns: { foo: () => 0 } });
-
     expect.assertions(1);
 
     try {
-      runt('This is invalid {$foo(}');
+      runty.string('This is invalid {$foo(}', { fns });
     } catch (e) {
       expect(e.type).toBe(RSyntaxErrorType.UNTERMINATED_FUNCTION);
     }
